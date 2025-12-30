11b90f3806910fc7369064e2c63d2d3e9c7f7838
topheart <jimlee0502@gmail.com>
2025-12-30 22:37:25 +0800

    Delete sql directory

--- sql/add_show_online_status.sql
+++
@@ -1,7 +0,0 @@
--- Add show_online_status column to wall_review_settings
-DO $$
-BEGIN
-    IF NOT EXISTS (SELECT 1 FROM information_schema.columns WHERE table_name = 'wall_review_settings' AND column_name = 'show_online_status') THEN
-        ALTER TABLE public.wall_review_settings ADD COLUMN show_online_status boolean NOT NULL DEFAULT true;
-    END IF;
-END $$;
--- sql/fix_review_settings_columns.sql
+++
@@ -1,4 +0,0 @@
--- Add missing columns to wall_review_settings table
-ALTER TABLE public.wall_review_settings
-ADD COLUMN IF NOT EXISTS marquee_enabled boolean NOT NULL DEFAULT true,
-ADD COLUMN IF NOT EXISTS show_online_status boolean NOT NULL DEFAULT true;
